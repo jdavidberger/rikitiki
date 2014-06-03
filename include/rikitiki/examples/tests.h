11c58df971f395090ba1ff9b808263e5087962a9
jberger <none@none>
2014-06-03 16:28:03 -0600

    test stuff

--- include/rikitiki/examples/tests.h
+++ include/rikitiki/examples/tests.h
@@ -8,7 +8,8 @@
 #include <mxcomp/vsostream.h>
 namespace rikitiki {
      namespace examples {
-          QUnit::UnitTest qunit(mxcomp::vsdb, QUnit::verbose);
+          std::stringstream test_results; 
+          QUnit::UnitTest qunit(test_results, QUnit::verbose);
           void Test(std::shared_ptr<Response> response) {
                std::string payload(response->response.str());
                QUNIT_IS_EQUAL(payload, "Basic Test!");
@@ -53,32 +54,29 @@ namespace rikitiki {
                     ctx << rikitiki::HttpStatus::Moved_Permanently;
                }
 
+               void operator () (ConnContextRef ctx) {
+                    // Note -- this is a race condition prone kind of thing....
+                    ctx << "<pre>" << test_results.str() << "</pre>";
+               }
+               void SetupTest(rikitiki::Server& server, const std::wstring& url, void(*testf)(std::shared_ptr<Response>)) {
+                    SimpleRequest request;
+                    request.uri = url;
+                    request.method = IRequest::GET;
+                    {
+                         std::shared_future<std::shared_ptr<Response>> future = server.ProcessRequest(request);
+                         std::async([=] { testf(future.get()); });
+                    }
+               }
                void Register(rikitiki::Server& server) OVERRIDE{
+                    server.AddHandler(CreateRoute<>::With(this, L"/"));
                     server.AddHandler(CreateRoute<>::With(this, L"/basictest", &TestsModule::BasicTest));
                     server.AddHandler(CreateRoute<int>::With(this, L"/querystringtest/{num}", &TestsModule::QueryStringTest));
                     server.AddHandler(CreateRoute<>::With(this, L"/headertest", &TestsModule::HeaderTest));
 
-                    SimpleRequest request;
-                    request.uri = L"basictest";
-                    request.method = IRequest::GET;
-                    {
-                         std::shared_future<std::shared_ptr<Response>> future = server.ProcessRequest(request);
-                         std::async([=] { Test(future.get()); });
-                    }
+                    SetupTest(server, L"basictest", &Test);
+                    SetupTest(server, L"querystringtest/42", &examples::QueryStringTest);
+                    SetupTest(server, L"headertest", &examples::HeaderTest);
                     
-                    request.uri = L"querystringtest/42";
-                    request.method = IRequest::GET;
-                    {
-                         std::shared_future<std::shared_ptr<Response>> future = server.ProcessRequest(request);
-                         std::async([=] { examples::QueryStringTest(future.get()); });
-                    }
-
-                    request.uri = L"headertest";
-                    request.method = IRequest::GET;
-                    {
-                         std::shared_future<std::shared_ptr<Response>> future = server.ProcessRequest(request);
-                         std::async([=] { examples::HeaderTest(future.get()); });
-                    }
                }
 
           };
